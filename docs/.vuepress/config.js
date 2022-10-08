e3524f77a411dbe67bdf30304e1aea999db63543
shhBeforever <1209630399@qq.com>
2022-10-08 17:07:09 +0800

    feat:finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
+    title: 'Mooc-UI',
     base: '/moocUI/',
     themeConfig:{
+        nav:[{
+            text: 'Home', link: '/'
+        },{
+            text: 'Github', link: 'https://github.com/shhBeforever/moocUI'
+        },{
+            text: 'Npm', link: 'https://www.npmjs.com/package/my-mooc-ui'
+        }],
         sidebar:[
             '/',
             '/componentDocs/card'
